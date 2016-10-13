4539d8b1842a030829ccbf6692b9d49946884ca4
muemich <michaelmuellerrw@gmail.com>
2016-10-13 07:37:39 +0200

    Minor fixes, rename 08

--- cicd/vagrant/nodejs_app/app.js
+++ cicd/vagrant/nodejs_app/app.js
@@ -4,7 +4,7 @@ var http = require('http');
 // Configure our http server to respond
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello\n");
+  response.end("Hello test\n");
 });
 
 //Listen on port 8000
